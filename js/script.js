e8dd35eb17aad5a3851d20985c60022634529155
Siarhei Haishun <117076914+SiarheiHaishun@users.noreply.github.com>
2023-01-18 02:54:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@ counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
 
+let first = document.getElementById("first");
+        let last = document.getElementById("last");
+        let text = document.getElementById("text");
+
+        window.addEventListener('scroll', function(){
+            var value = window.scrollY;
+
+            first.style.top = value * 0.7 + 'px';
+            last.style.bottom = value * 0.5 + 'px';
+            text.style.top = value * 1 + 'px';
+        });
+
